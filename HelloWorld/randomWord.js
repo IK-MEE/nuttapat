2bba97f112e9e7a74992c3020cacdf9ed1de6902
Nuttapat_Nunthapatpokin <71090743+IK-MEE@users.noreply.github.com>
2025-06-24 18:12:24 +0700

    Commit first portfolio folder

---
+++ HelloWorld/randomWord.js
@@ -0,0 +1,9 @@
+let wordList = [
+    "Awesome.",
+    "doing Good.",
+    "moving Forward.",
+    "the Best!"]
+
+
+let ranInt = Math.floor(Math.random() * wordList.length)
+console.log(`You are ${wordList[ranInt]}`)
